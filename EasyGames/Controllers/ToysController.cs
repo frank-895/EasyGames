3a5cd5f344c608f640234b7e3b038a55226696af
frank <frank@DESKTOP-EKQ6KKU>
2025-08-12 12:25:26 +0100

    Set up EasyGames project with default ASP.NET Core authentication. Created abstract Product base class and derived Book, Game, Toy models with data annotations. Scaffolded MVC controllers and views for all models using Entity Framework.

---
+++ EasyGames/Controllers/ToysController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EasyGames.Data;
+using EasyGames.Models;
+
+namespace EasyGames.Controllers
+{
+    public class ToysController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToysController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Toys
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Toy.ToListAsync());
+        }
+
+        // GET: Toys/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toy = await _context.Toy
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (toy == null)
+            {
+                return NotFound();
+            }
+
+            return View(toy);
+        }
+
+        // GET: Toys/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Toys/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,RecommendedAge,Material,Id,Price,StockQuantity")] Toy toy)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(toy);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toy);
+        }
+
+        // GET: Toys/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toy = await _context.Toy.FindAsync(id);
+            if (toy == null)
+            {
+                return NotFound();
+            }
+            return View(toy);
+        }
+
+        // POST: Toys/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,RecommendedAge,Material,Id,Price,StockQuantity")] Toy toy)
+        {
+            if (id != toy.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(toy);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ToyExists(toy.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toy);
+        }
+
+        // GET: Toys/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toy = await _context.Toy
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (toy == null)
+            {
+                return NotFound();
+            }
+
+            return View(toy);
+        }
+
+        // POST: Toys/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var toy = await _context.Toy.FindAsync(id);
+            if (toy != null)
+            {
+                _context.Toy.Remove(toy);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ToyExists(int id)
+        {
+            return _context.Toy.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EasyGames/Models/Class.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// NOTE - ? makes the field nullable
+
+namespace EasyGames.Models
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Precision(18, 2)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int StockQuantity { get; set; }
+    }
+
+    public class Book : Product
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string? Author { get; set; }
+
+        [MaxLength(100)]
+        public string? Genre { get; set; }
+    }
+
+    public class Game : Product
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string? Platform { get; set; }
+
+        [MaxLength(100)]
+        public string? AgeRating { get; set; }
+    }
+
+    public class Toy : Product
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string? RecommendedAge { get; set; }
+
+        [MaxLength(100)]
+        public string? Material { get; set; }
+    }
+}
